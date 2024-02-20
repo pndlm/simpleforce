c649f638011c50f42d447c50c7ca3c66b54f3efc
Colin Black <black@pndlm.com>
2024-02-20 14:13:30 -0800

    Add QueryRaw method to receive a raw response back from a SOQL query

--- force.go
+++ force.go
@@ -50,22 +50,23 @@ type QueryResult struct {
 
 // Expose sid to save in admin settings
 func (client *Client) GetSid() (sid string) {
-        return client.sessionID
+	return client.sessionID
 }
 
-//Expose Loc to save in admin settings
+// Expose Loc to save in admin settings
 func (client *Client) GetLoc() (loc string) {
 	return client.instanceURL
 }
 
 // Set SID and Loc as a means to log in without LoginPassword
 func (client *Client) SetSidLoc(sid string, loc string) {
-        client.sessionID = sid
-        client.instanceURL = loc
+	client.sessionID = sid
+	client.instanceURL = loc
 }
 
-// Query runs an SOQL query. q could either be the SOQL string or the nextRecordsURL.
-func (client *Client) Query(q string) (*QueryResult, error) {
+// QueryRaw runs a SOQL query and returns the raw result data.
+// q could either be the SOQL string or the nextRecordsURL.
+func (client *Client) QueryRaw(q string) ([]byte, error) {
 	if !client.isLoggedIn() {
 		return nil, ErrAuthentication
 	}
@@ -87,6 +88,17 @@ func (client *Client) Query(q string) (*QueryResult, error) {
 	data, err := client.httpRequest("GET", u, nil)
 	if err != nil {
 		log.Println(logPrefix, "HTTP GET request failed:", u)
+		return nil, err
+	}
+
+	return data, nil
+}
+
+// Query runs a SOQL query and returns a *QueryResult.
+// q could either be the SOQL string or the nextRecordsURL.
+func (client *Client) Query(q string) (*QueryResult, error) {
+	data, err := client.QueryRaw(q)
+	if err != nil {
 		return nil, err
 	}
 
@@ -332,7 +344,7 @@ func parseHost(input string) string {
 	return "Failed to parse URL input"
 }
 
-//Get the List of all available objects and their metadata for your organization's data
+// Get the List of all available objects and their metadata for your organization's data
 func (client *Client) DescribeGlobal() (*SObjectMeta, error) {
 	apiPath := fmt.Sprintf("/services/data/v%s/sobjects", client.apiVersion)
 	baseURL := strings.TrimRight(client.baseURL, "/")
